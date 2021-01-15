dbeee208971278d3b537b48e2025709b6779f7ce
Hamit Zor <hamit.zor@blickfeld.com>
2021-01-15 09:21:36 +0100

    Update paths

--- start-test-server.js
+++ start-test-server.js
@@ -5,5 +5,5 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname)));
 
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')));
 app.listen(4500, () => console.log('Local test server started at http://localhost:4500'));
